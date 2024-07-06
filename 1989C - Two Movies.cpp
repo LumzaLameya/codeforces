ebbb5493b8f163f9fd1533fbaf1c79647f933d0b
Lameya <124144887+LumzaLameya@users.noreply.github.com>
2024-07-06 21:36:47 +0600

    Create 1989C - Two Movies.cpp

---
+++ 1989C - Two Movies.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi(v) for (auto &it : v) cin >> it;
+#define vo(v) for (auto it : v) cout << it << " "; cout << endl;
+#define yout cout << "YES" << endl;
+#define nout cout << "NO" << endl;
+#define org(v) sort(v.begin(), v.end());
+#define rorg(v) sort(v.rbegin(), v.rend());
+#define rvr(v) reverse(v.begin(), v.end());
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / __gcd(a, b)))
+const ll mod = 1e9+7;
+const int N=1e5+5;
+const ll inf=3e18+9;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+
+
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+        int n;
+        cin>>n;
+        vector<int>a(n),b(n);
+        vi(a);
+        vi(b);
+        int x=0,y=0,neg=0,pos=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==b[i] && a[i]==1)
+                pos++;
+            else if(a[i]==b[i] && a[i]==-1)
+                neg++;
+            else if(a[i]>b[i])
+                x+=a[i];
+            else if(a[i]<b[i])
+                y+=b[i];
+        }
+        int c=INT_MIN;
+        for(int i=-neg;i<=pos;i++)
+        {
+            c=max(c,min(x+i,y+(pos-neg-i)));
+        }
+        cout<<c<<endl;
+    }
+    return 0;
+}
